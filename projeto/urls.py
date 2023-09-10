2f5d334ee78b86e7ded5477b9126845dd4b452bd
Robison Aleixo <robison.wilson@gmail.com>
2023-09-10 14:27:47 -0300

    Organizando a estrutura das pastas

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def my_view(request):
-    return HttpResponse('Sobre')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', include('recipes.urls')),
 ]
